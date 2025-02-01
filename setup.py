16e10e32b566b731368e68f3a24c218851824cd4
mdkamranalam <work.mdkamranalam@gmail.com>
2025-02-01 18:26:47 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MediBot',
+    version='v0.0',
+    author='Md. Kamran Alam',
+    author_email="work.mdkamranalam@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
